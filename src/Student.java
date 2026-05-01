d4c9dec69b692f69de92a16a8a8df1e8c7c63d5e
nathanmalone99@gmail.com <nathanmalone99@gmail.com>
2026-05-01 10:34:14 +0100

    Added a letter grade method to student to show the letter grade each student received

--- src/Student.java
+++ src/Student.java
@@ -12,12 +12,30 @@ public class Student {
          return name;
      }
 
-    public double getGrade() {
+     public double getGrade() {
         return grade;
+    }
+
+     public String getLetterGrade() {
+         if (grade >= 90) {
+             return "A";
+         }
+         else if (grade >= 75) {
+             return "B";
+         }
+         else if (grade >= 60) {
+             return "C";
+         }
+         else if (grade >= 40) {
+             return "D";
+         }
+         else {
+             return "F";
+         }
     }
 
     @Override
     public String toString() {
-         return name + " | Grade: " + grade;
+         return name + " | Grade: " + grade + " | :" + getLetterGrade();
     }
 }
